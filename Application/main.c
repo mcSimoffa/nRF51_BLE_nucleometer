21636d23fcf1deece1b788e19d051c1aab1c2a97
mcsimoff <mcsimoffa@yahoo.com>
2020-09-28 17:19:09 +0300

    BLE step liitle1

--- Application/main.c
+++ Application/main.c
@@ -35,64 +35,32 @@
 **********************************************************************
 
 -------------------------- END-OF-HEADER -----------------------------
-
-File    : main.c
-Purpose : Generic application start
-
 */
 #include "nrf.h"
 #include "boards.h"
-#include "nrf_delay.h"
-#include "nrf_gpio.h"
-#include "nrf_drv_gpiote.h"
-
-#include "nrf_drv_ppi.h"
+//#include "nrf_delay.h"
+//#include "nrf_gpio.h"
+//#include "nrf_drv_gpiote.h"
+#include "app_timer.h"
+#include "nrf_log_ctrl.h"
 #include <stdio.h>
 #include <stdlib.h>
-/*********************************************************************
-*
-*       main()
-*
-*  Function description
-*   Application entry point.
-*/
-int main(void) {
-nrf_gpio_cfg_input(BUTTON_1,NRF_GPIO_PIN_PULLUP);
-ret_code_t err_code;
-err_code = nrf_drv_ppi_init();
-err_code = nrf_drv_gpiote_init();
+// ********************************************************************
+#define DEAD_BEEF                       0xDEADBEEF  // Value used as error code on stack dump, can be used to identify stack location on stack unwind.
 
-nrf_ppi_channel_t ppi_channel;
-err_code = nrf_drv_ppi_channel_alloc(&ppi_channel); //aalocated chanells store in:  m_channels_allocated
+#define APP_TIMER_PRESCALER             0 // Value of the RTC1 PRESCALER register.
+#define APP_TIMER_OP_QUEUE_SIZE         4 // Size of timer operation queues
 
-  
-  nrf_gpiote_task_configure(0,GREEN_LED,NRF_GPIOTE_POLARITY_TOGGLE,1);
-  nrf_gpiote_event_configure(1,BUTTON_1,NRF_GPIOTE_POLARITY_LOTOHI);
 
-uint32_t eventaddr = nrf_gpiote_event_addr_get(NRF_GPIOTE_EVENTS_IN_1);
-uint32_t taskaddr = nrf_gpiote_task_addr_get(NRF_GPIOTE_TASKS_OUT_0);
-err_code = nrf_drv_ppi_channel_assign(ppi_channel, eventaddr, taskaddr); //it assign event addr in CH[x].EEP and task addr in CH[x].TEP
-err_code = nrf_drv_ppi_channel_enable(ppi_channel); //it set bit in CHSET
+int main(void)
+{
+   uint32_t err_code;
+    // Initialize.
+    err_code = NRF_LOG_INIT(NULL);
+    APP_ERROR_CHECK(err_code);
 
-  nrf_gpiote_task_enable(0);
-  nrf_gpiote_event_enable(1);
-  int i;
-/*
-  nrf_gpio_cfg_output(ORANGE_LED);
-  nrf_gpio_cfg_output(GREEN_LED);
-  for (uint8_t i = 0; i < 100; i++) {
-    printf("Hello World %d!\n", i);
-    nrf_gpio_pin_set(ORANGE_LED);
-    nrf_delay_ms(100);
-    nrf_gpio_pin_clear(ORANGE_LED);
-    nrf_gpio_pin_toggle(GREEN_LED);
-    nrf_delay_ms(100);
-  }*/
-
-  do {
-  asm("nop");
-  //nrf_delay_ms(100);
-  } while (1);
+    APP_TIMER_INIT(APP_TIMER_PRESCALER, APP_TIMER_OP_QUEUE_SIZE, false); //what it does ?
+    err_code = bsp_init(BSP_INIT_LED, APP_TIMER_TICKS(100, APP_TIMER_PRESCALER), NULL);
 }
 
 /*************************** End of file ****************************/
