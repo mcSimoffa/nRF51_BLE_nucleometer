14e7418e72fa2313a0f57c0d6e7f28dad51f219d
mcsimoff <mcsimoffa@yahoo.com>
2020-09-29 22:28:36 +0300

    gluk in memory allocation

--- Application/main.c
+++ Application/main.c
@@ -37,13 +37,14 @@
 */
 #include <stdio.h>
 #include <stdlib.h>
-#include "app_timer.h"
 #include "boards.h"
 #include "bsp.h"
 #include "custom_board.h"
 #include "nrf.h"
+#include "nrf_log.h"
 #include "nrf_log_ctrl.h"
 #include "softdevice_handler.h"
+#include "app_timer.h"
 #include "ble.h"
 #include "softdevice_handler.h"
 #include "ble_advdata.h"
@@ -58,15 +59,53 @@
 static void ble_stack_init(void);
 static void advertising_init(void);
 
-// ********************************************************************
+/* *******************************************************************
+                    DEFINITIONs
+******************************************************************** */
 //#define DEAD_BEEF 0xDEADBEEF      // Value used as error code on stack dump, can be used to identify stack location on stack unwind.
 #define APP_TIMER_PRESCALER     0   // Value of the RTC1 PRESCALER register.
 #define APP_TIMER_OP_QUEUE_SIZE 4   // Size of timer operation queues
-#define CENTRAL_LINK_COUNT      0   // Number of central links used by the application. When changing this number remember to adjust the RAM settings*/
-#define PERIPHERAL_LINK_COUNT   0   // Number of peripheral links used by the application. When changing this number remember to adjust the RAM settings*/
+#define CENTRAL_LINK_COUNT      0   // Number of central links used by the application. When changing this number remember to adjust the RAM settings
+#define PERIPHERAL_LINK_COUNT   0   // Number of peripheral links used by the application. When changing this number remember to adjust the RAM settings
 //in global defines needs BLE_STACK_SUPPORT_REQD https://infocenter.nordicsemi.com/index.jsp?topic=%2Fcom.nordic.infocenter.sdk5.v12.3.0%2Flib_softdevice_handler.html
-#define APP_COMPANY_IDENTIFIER          0x0059                            /**< Company identifier for Nordic Semiconductor ASA. as per www.bluetooth.org. */
+#define APP_COMPANY_IDENTIFIER          0x0059   // Company identifier for Nordic Semiconductor ASA. as per www.bluetooth.org.
 
+#define APP_BEACON_INFO_LENGTH          23      // Total length of information advertised by the Beacon.
+#define APP_DEVICE_TYPE                 0x02    // 0x02 refers to Beacon.
+#define APP_ADV_DATA_LENGTH             21      //Length of manufacturer specific data in the advertisement.
+#define APP_BEACON_UUID                 0x01, 0x12, 0x23, 0x34, \
+                                        0x45, 0x56, 0x67, 0x78, \
+                                        0x89, 0x9a, 0xab, 0xbc, \
+                                        0xcd, 0xde, 0xef, 0xf0    // Proprietary UUID for Beacon.
+
+#define APP_MAJOR_VALUE                 0x01, 0x02    // Major value used to identify Beacons.
+#define APP_MINOR_VALUE                 0x03, 0x04    // Minor value used to identify Beacons.
+#define APP_MEASURED_RSSI               0xC3          // The Beacon's measured RSSI at 1 meter distance in dBm.
+#define NON_CONNECTABLE_ADV_INTERVAL    MSEC_TO_UNITS(100, 625) // The advertising interval for non-connectable advertisement (100 ms). This value can vary between 100ms to 10.24s). Step 625 uS
+#define APP_CFG_NON_CONN_ADV_TIMEOUT    0                                 /**< Time for which the device must be advertising in non-connectable mode (in seconds). 0 disables timeout. */
+
+
+/* *******************************************************************
+                    Global Variable
+******************************************************************** */
+static uint8_t m_beacon_info[APP_BEACON_INFO_LENGTH] =   //Information advertised by the Beacon.
+{
+    APP_DEVICE_TYPE,     // Manufacturer specific information. Specifies the device type in this
+                         // implementation.
+    APP_ADV_DATA_LENGTH, // Manufacturer specific information. Specifies the length of the
+                         // manufacturer specific data in this implementation.
+    APP_BEACON_UUID,     // 128 bit UUID value.
+    APP_MAJOR_VALUE,     // Major arbitrary value that can be used to distinguish between Beacons.
+    APP_MINOR_VALUE,     // Minor arbitrary value that can be used to distinguish between Beacons.
+    APP_MEASURED_RSSI    // Manufacturer specific information. The Beacon's measured TX power in
+                         // this implementation.
+};
+
+static ble_gap_adv_params_t m_adv_params;   // Parameters to be passed to the stack when starting advertising.
+
+/* *******************************************************************
+                    MAIN program cycle
+******************************************************************** */
 int main(void) 
 { //it here from C:\Program Files\SEGGER\SEGGER Embedded Studio for ARM 5.10a\source\thumb_crt0.s
   uint32_t err_code;
@@ -80,7 +119,17 @@ int main(void)
   APP_ERROR_CHECK(err_code);
   ble_stack_init();
   advertising_init();
+  for (;; )
+    {
+        if (NRF_LOG_PROCESS() == false)
+        {
+            //power_manage();
+        }
+    }
 }
+
+
+
 
 /* ******************************************************************
 Initializes the SoftDevice and the BLE event interrupt.
